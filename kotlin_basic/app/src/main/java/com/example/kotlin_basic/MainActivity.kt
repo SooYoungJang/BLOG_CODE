6845a3bfe24fb015267481eca0646cb43a6b2f2d
SooYoungJang <tturrr10@gmail.com>
2021-12-03 22:57:21 +0900

    클래스와 프로퍼티

--- kotlin_basic/app/src/main/java/com/example/kotlin_basic/MainActivity.kt
+++ kotlin_basic/app/src/main/java/com/example/kotlin_basic/MainActivity.kt
@@ -2,25 +2,14 @@ package com.example.kotlin_basic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.kotlin_basic.FunctionsAndVariables.ClassAndProperty
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println(funName("word"))
 
-        val question = "삶, 우주, 그리고 모든 것에 대한 궁극적인 질문"
-        val question1: String = "삶, 우주, 그리고 모든 것에 대한 궁극적인 질문"
-
-        val languages = arrayListOf("java")
-        languages.add("kotlin")
-
-    }
-
-    fun funName(word: String) : String {
-        println("Hello World")
-        val fullWord = word + " in English"
-        return fullWord
     }
 
 
